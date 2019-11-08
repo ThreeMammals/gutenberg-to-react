7bcaf7adf38a9fc0224efa0e90cd0f44d05df321
Tom Pallister <TomPallister@users.noreply.github.com>
2019-11-08 21:36:32 +0000

    build prs

--- packages/scripts/build.sh
+++ packages/scripts/build.sh
@@ -9,4 +9,15 @@ git config --global user.name "TomPallister"
 npm install
 lerna bootstrap
 lerna run test --stream
-lerna publish --conventional-commits --changelog-preset angular --yes
+
+branch=$(git symbolic-ref --short -q HEAD)
+master_branch=master
+
+if [ $branch != $master_branch ]
+then
+    echo "PR PUBLISH"
+    lerna publish --canary --conventional-commits --changelog-preset angular --yes
+else
+    echo "MASTER PUBLISH"
+    lerna publish --conventional-commits --changelog-preset angular --yes
+fi
